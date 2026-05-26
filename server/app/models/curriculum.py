c8d88f02446867bff109e60b5abdec1bad476054
MunDongHyun <zmdk101@gmail.com>
2026-05-26 12:34:35 +0900

    커리큘럼 로직 이것저것 수정함

--- server/app/models/curriculum.py
+++ server/app/models/curriculum.py
@@ -18,6 +18,7 @@ class Curriculum(Base):
     cur_week_plan: Mapped[dict | list | None] = mapped_column(JSON, nullable=True)
     cur_assigned_learner_ids: Mapped[dict | None] = mapped_column(JSON, nullable=True)
     cur_status: Mapped[str | None] = mapped_column(Enum("active", "unactive"), nullable=True, default="active")
+    cur_deadline: Mapped[DateTime | None] = mapped_column(DateTime, nullable=True, comment="마감일")
     cur_created_at: Mapped[DateTime | None] = mapped_column(DateTime, nullable=True, server_default=func.now())
     cur_updated_at: Mapped[DateTime] = mapped_column(
         DateTime,
